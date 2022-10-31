d22937cc6b86a261ef109999ba7def8e5f5cc67b
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-10-31 16:13:18 +0800

    'first'

---
+++ WastageWreckerBackend/src/main/java/com/WastageWreckerBackend/WastageWreckerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.WastageWreckerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastageWreckerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastageWreckerBackendApplication.class, args);
+	}
+
+}
---
+++ WastageWreckerBackend/src/test/java/com/WastageWreckerBackend/WastageWreckerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WastageWreckerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastageWreckerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
